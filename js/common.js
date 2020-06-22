79079ca05f46e7caa5a6cc218d33f15547e43e55
Alexanderwebsot <64553081+Alexanderwebsot@users.noreply.github.com>
2020-06-22 17:50:44 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -3,6 +3,15 @@ $(document).ready(function () {
   $('.select-beast').selectize({
       sortField: 'text'
   });
+  $('.catalog-block__img_slider').slick({
+    arrows: null,
+    asNavFor: '.catalog-content__block_slider'
+  })
+  $('.catalog-content__block_slider').slick({
+    slidesToShow: 5,
+    focusOnSelect: true,
+    asNavFor: '.catalog-block__img_slider',
+  })
   $('.nav__menu__wrapper').on('click', function() {
   	let menu = $('.menu-block');
   	$(menu).toggleClass('menu-block__active');
@@ -16,4 +25,34 @@ $(document).ready(function () {
   		$(this).addClass('current');
   		$("#"+tab_id).addClass('current');
   	})
+  $('.on-c').on('click', function() {
+    return false;
+  })
 });
+$(document).ready(function() {
+    $(".set > a").on("click", function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this)
+          .siblings(".content")
+          .slideUp(200);
+        $(".set > a i")
+          .removeClass("fa-minus")
+          .addClass("fa-plus");
+      } else {
+        $(".set > a i")
+          .removeClass("fa-minus")
+          .addClass("fa-plus");
+        $(this)
+          .find("i")
+          .removeClass("fa-plus")
+          .addClass("fa-minus");
+        $(".set > a").removeClass("active");
+        $(this).addClass("active");
+        $(".content").slideUp(200);
+        $(this)
+          .siblings(".content")
+          .slideDown(200);
+      }
+    });
+  });
